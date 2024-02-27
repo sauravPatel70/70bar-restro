738c8b2ef8acc3a60adb25f9091d62796e067d7c
SAURAV KUMAR <148654892+sauravPatel70@users.noreply.github.com>
2024-02-27 19:40:42 +0530

    bar and restrto
    bar and restro

---
+++ restuarent.js
@@ -0,0 +1,51 @@
+// search button and cart button 
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+// search button and cart button end
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+  
+    if(window.scrollY > 60){
+      document.querySelector('#scroll-top').classList.add('active');
+    }else{
+      document.querySelector('#scroll-top').classList.remove('active');
+    }
+  
+}
+  
+  function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+  
+  function fadeOut(){
+    setInterval(loader, 3000);
+  }
+  
+  window.onload = fadeOut();
+
+
+ 
